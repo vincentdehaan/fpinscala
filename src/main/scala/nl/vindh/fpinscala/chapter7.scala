514cd237f258f2d3342fdcba1f2acb0d9a000346
Vincent de Haan <vincentdehaan@gmail.com>
2019-07-13 21:36:12 +0200

    Chapter 7 done

--- src/main/scala/nl/vindh/fpinscala/chapter7.scala
+++ src/main/scala/nl/vindh/fpinscala/chapter7.scala
@@ -2,12 +2,10 @@ package nl.vindh.fpinscala
 
 import java.util.concurrent.{Callable, ExecutorService, Future, TimeUnit}
 
+import scala.collection.immutable.{List => ScalaList, Nil => ScalaNil}
+
 object chapter7 extends App {
-
-
-
   type Par[A] = ExecutorService => Future[A]
-
 
   object Par {
     def unit[A](a: A): Par[A] = _ => UnitFuture(a)
@@ -18,7 +16,6 @@ object chapter7 extends App {
       def isCancelled: Boolean = false
       def cancel(evenIfRunning: Boolean): Boolean = false
     }
-
 
     // Exercise 7.1
     def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] = {
@@ -44,6 +41,9 @@ object chapter7 extends App {
       }
     }
 
+    def map[A, B](pa: Par[A])(f: A => B): Par[B] =
+      map2(pa, unit(()))((a, _) => f(a))
+
     def fork[A](a: => Par[A]): Par[A] =
       es => es.submit(new Callable[A] {
         def call = a(es).get
@@ -53,6 +53,67 @@ object chapter7 extends App {
 
     // Exercise 7.4
     def asyncF[A, B](f: A => B): A => Par[B] = a => lazyUnit(f(a))
+
+    // Exercise 7.5
+    def sequence[A](ps: ScalaList[Par[A]]): Par[ScalaList[A]] =
+      ps match {
+        case ScalaNil => unit(ScalaNil.asInstanceOf[ScalaList[A]])
+        case hd :: tl => map2(hd, sequence(tl))((phd, ptl) => phd :: ptl)
+      }
+
+    def parMap[A, B](ps: ScalaList[A])(f: A => B): Par[ScalaList[B]] = fork {
+      val fbs: ScalaList[Par[B]] = ps.map(asyncF(f))
+      sequence(fbs)
+    }
+
+    // Exercise 7.6
+    def parFilter[A](as: ScalaList[A])(f: A => Boolean): Par[ScalaList[A]] = {
+      val filt = Par.parMap(as)(a => if (f(a)) ScalaList(a) else ScalaNil.asInstanceOf[ScalaList[A]])
+      Par.map(filt)(_.flatten)
+    }
+
+    // Exercise 7.11
+    def choiceN[A](n: Par[Int])(choices: ScalaList[Par[A]]): Par[A] =
+      es => {
+        val idx = run(es)(n).get
+        choices(idx)(es)
+      }
+
+    def choice[A](cond: Par[Boolean])(t: Par[A], f: Par[A]): Par[A] =
+      choiceN(map(cond)(if(_) 1 else 0))(ScalaList(f, t))
+
+    // Exercise 7.12
+    def choiceMap[K, V](key: Par[K])(choices: Map[K, Par[V]]): Par[V] =
+      es => {
+        val key = run(es)(key).get
+        choices(key)(es)
+      }
+
+    // Exercise 7.13
+    def chooser[A, B](pa: Par[A])(choices: A => Par[B]): Par[B] =
+      es => {
+        val key = run(es)(pa).get
+        choices(key)(es)
+      }
+
+    def choiceWithChooser[A](cond: Par[Boolean])(t: Par[A], f: Par[A]): Par[A] =
+      chooser(cond)(if(_) t else f)
+
+    def choiceNWithChooser[A](n: Par[Int])(choices: ScalaList[Par[A]]): Par[A] =
+      chooser(n)(choices(_))
+
+    // Exercise 7.14
+    def join[A](a: Par[Par[A]]): Par[A] =
+      es => {
+        val p = run(es)(a).get
+        p(es)
+      }
+
+    def flatMap[A, B](a: Par[A])(f: A => Par[B]): Par[B] =
+      join(map(a)(f))
+
+    def join_[A](a: Par[Par[A]]): Par[A] =
+      es => flatMap(a)(x => x)(es)
 
     def run[A](s: ExecutorService)(a: Par[A]): Future[A] = a(s)
   }
--- src/test/scala/nl/vindh/fpinscala/chapter7Test.scala
+++ src/test/scala/nl/vindh/fpinscala/chapter7Test.scala
@@ -1,10 +1,11 @@
 package nl.vindh.fpinscala
 
-import java.util.concurrent.{Callable, ForkJoinPool, TimeUnit}
+import java.util.concurrent.{Callable, Executors, ForkJoinPool, TimeUnit}
 
 import org.scalatest.{FlatSpec, Matchers}
 import java.util.Calendar
 
+import scala.collection.immutable.{List => ScalaList}
 import chapter7._
 
 import scala.util.Try
@@ -18,6 +19,13 @@ class chapter7Test extends FlatSpec with Matchers {
       a
     }
   })
+
+  def slowFilter(ms: Int): Int => Boolean = i => {
+    println(s"${Thread.currentThread().getName}: ${Calendar.getInstance.getTime}: Filter sleeping")
+    Thread.sleep(ms)
+    println(s"${Thread.currentThread().getName}: ${Calendar.getInstance.getTime}: Filter waking up")
+    i % 2 == 0
+  }
 
   "get" should "respect the timeout" in {
     // Arrange
@@ -47,5 +55,53 @@ class chapter7Test extends FlatSpec with Matchers {
     // Assert
     assert(withTimeout.isFailure)
     assert(withoutTimeout === Try(3))
+  }
+
+  "sequence" should "work as expected" in {
+    // Arrange
+    val ps = ScalaList(Par.unit(1), Par.unit(2), Par.unit(3))
+
+    // Act
+    val seq = Par.sequence(ps)
+
+    // Assert
+    val es = new ForkJoinPool
+    // assert(seq === Par.unit(ScalaList(1, 2, 3)))
+    // This does not work because function expressions do not have suitable equality functions
+    assert(Par.run(es)(seq).get === ScalaList(1, 2, 3))
+  }
+
+  "Par.parFilter" should "run the filter in parallel" in {
+    // Arrange
+    val as = ScalaList(1, 2, 3)
+
+    // Act
+    val f = Par.parFilter(as)(slowFilter(1000))
+
+    // Assert
+    val es = new ForkJoinPool
+    assert(Try(Par.run(es)(f).get(1100, TimeUnit.MILLISECONDS)) === Try(ScalaList(2)))
+  }
+
+  // Exercise 7.9
+  "fork" should "deadlock in fixed size thread pool" in {
+    // Arrange
+    def recursiveFork(n: Int): Par[Int] =
+      if(n > 0) Par.fork(recursiveFork(n - 1))
+      else Par.unit(42)
+    val es7 = Executors.newFixedThreadPool(6)
+    val par5 = recursiveFork(5)
+    val par6 = recursiveFork(6)
+    val par7 = recursiveFork(7)
+
+    // Act
+    val get5 = Try(Par.run(es7)(par5).get(1000, TimeUnit.MILLISECONDS))
+    val get6 = Try(Par.run(es7)(par6).get(1000, TimeUnit.MILLISECONDS))
+    val get7 = Try(Par.run(es7)(par7).get(1000, TimeUnit.MILLISECONDS))
+
+    // Assert
+    assert(get5 === Try(42))
+    assert(get6 === Try(42))
+    assert(get7.isFailure)
   }
 }
